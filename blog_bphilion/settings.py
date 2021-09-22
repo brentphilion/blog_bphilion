6f7751561b2522ec24bb0addc065f5d22d642ca6
brentphilion <43643086+brentphilion@users.noreply.github.com>
2021-09-22 08:50:40 -0400

    connects to postgres

--- blog_bphilion/settings.py
+++ blog_bphilion/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,10 +75,9 @@ WSGI_APPLICATION = 'blog_bphilion.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:postgres@127.0.0.1:5432/blog_bphilion'
+    )
 }
 
 
